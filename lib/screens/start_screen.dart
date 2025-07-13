c933d357d2dd1a69591453c4613fe19430115bb2
CarlitooooK <carlocabrera8485@gmail.com>
2025-07-13 13:18:41 -0600

    AppRelease changes

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -11,6 +11,7 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
+  bool obscureText = true;
   TextEditingController myController = TextEditingController();
 
   void checkUser() {
@@ -18,6 +19,7 @@ class _StartScreenState extends State<StartScreen> {
       Navigator.pushNamed(context, "/homepage");
     } else {
       showDialog<String>(
+        animationStyle: AnimationStyle(duration: Duration(milliseconds: 550)),
         context: context,
         builder: (BuildContext context) => AlertDialog(
           title: const Text('Usuario Incorrecto'),
@@ -105,6 +107,7 @@ class _StartScreenState extends State<StartScreen> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
+          SizedBox(height: 20),
 
           // Campo de texto
           Padding(
@@ -114,15 +117,44 @@ class _StartScreenState extends State<StartScreen> {
               left: 30,
               right: 30,
             ),
-            child: TextField(
-              controller: myController,
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              maxLength: 8,
-              decoration: InputDecoration(
-                hintText: "Usuario",
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.remove_red_eye_rounded),
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.fromLTRB(0, 3, 20, 0),
+              margin: EdgeInsets.only(left: 20, right: 20),
+              height: 60,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                cursorColor: Colors.black,
+                obscureText: obscureText,
+                controller: myController,
+                textInputAction: TextInputAction.search,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      obscureText ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        obscureText = !obscureText;
+                      });
+                    },
+                  ),
+                  prefixIcon: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.person, color: Colors.black),
+                    color: Colors.grey,
+                  ),
+                  hintText: 'Correo Electrónico',
+                  hintStyle: TextStyle(color: Colors.black),
+                ),
+                style: TextStyle(color: Colors.black, fontSize: 18),
               ),
             ),
           ),
@@ -130,7 +162,7 @@ class _StartScreenState extends State<StartScreen> {
           // Botón
           Center(
             child: SizedBox(
-              width: 200,
+              width: 250,
               height: 50,
               child: ElevatedButton.icon(
                 icon: Icon(Icons.arrow_forward, color: Colors.white),
@@ -140,7 +172,7 @@ class _StartScreenState extends State<StartScreen> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: AppColors.buttonColor,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(32),
                   ),
                 ),
                 label: Text(
