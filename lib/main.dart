a9a2a4f0ca51d2fd2fa6f98962da0efe159fc2e3
CarlitooooK <cabreracarlo8485@gmail.com>
2025-09-15 12:45:52 -0600

    Try push

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,8 @@ import 'package:finestra_app/screens/start_screen.dart';
 import 'package:finestra_app/features/navigation/account_screen.dart';
 import 'package:finestra_app/features/navigation/settings_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: 'https://qyelvkeiumcuiovckurk.supabase.co', // Reemplaza con tu URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5ZWx2a2VpdW1jdWlvdmNrdXJrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTY1NDUxNiwiZXhwIjoyMDcxMjMwNTE2fQ.6ZT_HYOdkXZVgyXpVqrp2oZALS8vmZFFhLWfbSTbxVI', // Reemplaza con tu clave anónima
-  );
+void main() {
   runApp(const MyApp());
 }
 
